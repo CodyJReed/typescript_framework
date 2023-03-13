73bc7f88d492520d0d86f84fb0c2ba795e2978a9
CodyJReed <cody2j2reed@gmail.com>
2023-03-13 11:43:53 -0700

    type_frame import userShow and userForm for use with onRender()

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,40 +1,65 @@
-import { Model, HasId} from "../models/Model";
+import { Model, HasId } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K extends HasId> {
-    constructor(public parent: Element, public model: T) {
-        this.bindModel();
-      }
+  regions: {
+    [key: string]: Element;
+  } = {};
 
-      abstract template(): string
+  constructor(public parent: Element, public model: T) {
+    this.bindModel();
+  }
 
-      eventsMap(): {[key: string]: () => void} {
-        return {}
-      }
-    
-      bindModel(): void {
-        this.model.on("change", () => this.render());
-      }
+  abstract template(): string;
 
-      bindEvents(fragment: DocumentFragment): void {
-        const eventsMap = this.eventsMap();
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
+
+  bindModel(): void {
+    this.model.on("change", () => this.render());
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((el) => {
+        el.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const el = fragment.querySelector(selector);
+      if (el) this.regions[key] = el;
+    }
+  }
+
+  onRender(): void {
     
-        for (let eventKey in eventsMap) {
-          const [eventName, selector] = eventKey.split(":");
-    
-          fragment.querySelectorAll(selector).forEach((el) => {
-            el.addEventListener(eventName, eventsMap[eventKey]);
-          });
-        }
-      }
-    
-      render(): void {
-        this.parent.innerHTML = "";
-    
-        const templateElement = document.createElement("template");
-        templateElement.innerHTML = this.template();
-    
-        this.bindEvents(templateElement.content);
-    
-        this.parent.append(templateElement.content);
-      }
+  }
+
+  render(): void {
+    this.parent.innerHTML = "";
+
+    const templateElement = document.createElement("template");
+    templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender()
+
+    this.parent.append(templateElement.content);
+  }
 }
