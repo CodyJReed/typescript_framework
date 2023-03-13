42c629cd344ee9469787859061ae44fee3f6b039
CodyJReed <cody2j2reed@gmail.com>
2023-03-13 12:10:32 -0700

    type_frame add extra collectionview.ts exercise

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+// import { User } from "./models/User";
+// import { UserEdit } from "./views/UserEdit";
 
-const user = User.buildUser({ name: "Bindhi", age: 42 });
+// const user = User.buildUser({ name: "Bindhi", age: 42 });
 
-const root = document.getElementById("root");
+// const root = document.getElementById("root");
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
 
-  userEdit.render();
+//   userEdit.render();
 
-  console.log(userEdit)
-} else {
-    throw new Error('Root element not found')
-}
+//   console.log(userEdit)
+// } else {
+//     throw new Error('Root element not found')
+// }
+
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
+
+const collection = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+collection.on("change", () => {
+  const root = document.getElementById("root");
+
+  if (root) new UserList(root, collection).render();
+});
+
+collection.fetch();
